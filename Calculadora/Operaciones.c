45eed0cec12b0ba47a58cca2df8dbf2ee0106abc
josemiguel1014 <josemiguel@PC-CP>
2023-02-07 21:25:28 -0500

    Version 1

---
+++ Calculadora/Operaciones.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <math.h>
+
+// function definition
+
+double Suma(double a, double b)
+{
+    return a + b;
+}
+
+double Resta(double a, double b)
+{
+    return a - b;
+}
+
+double Multiplicacion(double a, double b)
+{
+    return a * b;
+}
+
+double Division(double a, double b)
+{
+    return a / b;
+}
+
+double RaizCuadrada(double a)
+{
+    return sqrt(a);
+}
+
+/*
+    @param a primer elemento
+    @return sen(a)
+*/
+double Seno(double a)
+{
+    return sin(a);
+}
+
+/*
+    @param a primer elemento
+    @return cos(a)
+*/
+double Coseno(double a)
+{
+    return cos(a);
+}
+
+/*
+    @param a primer elemento
+    @return tan(a)
+*/
+double Tangente(double a)
+{
+    return tan(a);
+}
+
+double Potencia(double a, double b)
+{
+    return pow(a, b);
+}
